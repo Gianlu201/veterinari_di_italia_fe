63a1bf2a9cf89eeaaa3966245470fcbd78acf0bc
Gianlu201 <gianlucadidiego2001@gmail.com>
2025-04-03 15:16:49 +0200

    firs impagination

--- src/components/PageVeterinarioComponent.jsx
+++ src/components/PageVeterinarioComponent.jsx
@@ -1,6 +1,32 @@
+import { useEffect } from 'react';
 import { Container } from 'react-bootstrap';
 
 const PageVeterinarioComponet = () => {
+  const getAllAnimals = async () => {
+    try {
+      const response = await fetch(
+        'https://localhost:7019/api/AnagraficaAnimale',
+        {
+          method: 'GET',
+        }
+      );
+
+      if (response.ok) {
+        const data = await response.json();
+
+        console.log(data);
+      }
+
+      // const data = await response.json();
+    } catch {
+      // finale cattivo :(
+    }
+  };
+
+  useEffect(() => {
+    getAllAnimals();
+  }, []);
+
   return (
     <Container>
       <h1>Pagina veterinario</h1>

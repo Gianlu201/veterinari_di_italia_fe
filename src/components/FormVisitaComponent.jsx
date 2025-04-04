c944a5e0c542610d9d90c8f4c2505698dd25b075
Reyett___ <reyrenna4@gmail.com>
2025-04-04 12:03:29 +0200

    visite-post

--- src/components/FormVisitaComponent.jsx
+++ src/components/FormVisitaComponent.jsx
@@ -4,6 +4,30 @@ import { useEffect, useState } from "react";
 
 const FormVisitaComponent = () => {
   const [Farmaci, setFarmaci] = useState([]);
+  const [DataVisita, setDataVisita] = useState("");
+  const [EsameObbiettivo, setEsameObbiettivo] = useState("");
+  const [Descrizione, setDescrizione] = useState("");
+  const [Farmaco, setFarmaco] = useState([]);
+
+  const PostVisita = async () => {
+    try {
+      const Visita = {
+        DataVisita: DataVisita,
+        EsameObbiettivo: EsameObbiettivo,
+        Descrizione: Descrizione,
+        Farmaco: [{ Farmaco }],
+      };
+      const response = await fetch("https://localhost:7019/api/Visite", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(Visita),
+      });
+    } catch {
+      throw new Error();
+    }
+  };
 
   const GetFarmaci = async () => {
     const response = await fetch("https://localhost:7019/api/Farmaci", {
@@ -23,12 +47,24 @@ const FormVisitaComponent = () => {
     GetFarmaci();
   }, []);
   return (
-    <form>
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        PostVisita();
+      }}
+    >
       <div className="mb-3">
         <label htmlFor="DataVisita" className="form-label">
           Data Visita
         </label>
-        <input type="datetime-local" className="form-control" id="dataVisita" />
+        <input
+          type="datetime-local"
+          className="form-control"
+          id="dataVisita"
+          onChange={(e) => {
+            setDataVisita(e.target.value);
+          }}
+        />
       </div>
       <div className="mb-3">
         <label htmlFor="EsameObbiettivo" className="form-label">
@@ -39,6 +75,9 @@ const FormVisitaComponent = () => {
           className="form-control"
           id="esameObbiettivo"
           placeholder="Inserisci qui un esame obbiettivo"
+          onChange={(e) => {
+            setEsameObbiettivo(e.target.value);
+          }}
         />
       </div>
       <div className="mb-3">
@@ -50,13 +89,24 @@ const FormVisitaComponent = () => {
           className="form-control"
           id="Descrizione"
           placeholder="Inserisci qui un'eventuale descrizione della visita"
+          onChange={(e) => {
+            setDescrizione(e.target.value);
+          }}
         />
       </div>
       <div className="mb-3">
         <label htmlFor="Farmaco" className="form-label">
           Farmaco
         </label>
-        <select className="form-select" id="farmaco" multiple size={5}>
+        <select
+          className="form-select"
+          id="farmaco"
+          multiple
+          size={5}
+          onChange={(e) => {
+            setFarmaco(e.target.value);
+          }}
+        >
           {Farmaci.length > 0 &&
             Farmaci.map((element) => {
               return (
